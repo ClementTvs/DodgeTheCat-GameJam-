028dd4d3919f3a74a40a0d8b19583cb11520512e
unknown <arthur.perez66000@gmail.com>
2025-08-26 11:46:59 +0200

    add timer

--- Assets/src/Jar.cs
+++ Assets/src/Jar.cs
@@ -3,11 +3,17 @@ using UnityEngine;
 public class Jar : MonoBehaviour
 {
     static private int cheeseSaved = 0;
+    bool ending = true;
     private void OnCollisionEnter2D(Collision2D collision)
     {
         Debug.Log("a");
         cheeseSaved += GameManager.Instance.getCheese();
         GameManager.Instance.resetCheese();
+        if (cheeseSaved >= 10 && ending == true)
+        {
+            Debug.Log("You won !");
+            ending = false;
+        }
     }
 
     static public int getCheeseSaved()
--- Assets/src/UIManager.cs
+++ Assets/src/UIManager.cs
@@ -5,6 +5,8 @@ public class UIManager : MonoBehaviour
     public TextMeshProUGUI cheeseNbrTextShelter;
     public TextMeshProUGUI cheeseNbrTextCheese;
     public TextMeshProUGUI cheeseNbrSavedText;
+    public TextMeshProUGUI timerText;
+    private float timeElapsed = 0f;
 
     void Update()
     {
@@ -14,5 +16,17 @@ public class UIManager : MonoBehaviour
         cheeseNbrTextCheese.text = cheeseNbr.ToString();
         cheeseNbrTextShelter.text = cheeseNbr.ToString();
         cheeseNbrSavedText.text = cheeseSaved.ToString();
+        if (Teleportation.isInShelter == false)
+        {
+            timeElapsed += Time.deltaTime;
+
+            int minutes = Mathf.FloorToInt(timeElapsed / 60);
+            int secondes = Mathf.FloorToInt(timeElapsed % 60);
+
+            timerText.text = string.Format("{0:00}:{1:00}", minutes, secondes);
+        }
+        else
+            timeElapsed = 0f;
+
     }
 }
